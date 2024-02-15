29c45dfe35944c25c0841e1808a2baae68a574c5
Dmitriy <neitdirect@yandex.ru>
2024-02-15 14:05:16 +0600

    15.02.24 Dima

---
+++ Assets/Scripts/GameStatesChanger.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameStateObjects
+    {
+        [SerializeField] private GameObject[] _objects;
+
+        public IEnumerable<GameObject> Objects => _objects;
+    }
+
+    public class GameStatesChanger : MonoBehaviour
+    {
+        [SerializeField] private GameStateObjects _quizGame;
+        [SerializeField] private GameStateObjects _lettersGame;
+        [SerializeField] private GameStateObjects _objectsGame;
+        [SerializeField] private GameStateObjects _menuObjects;
+
+        private GameStateObjects _currentGameStateObjects;
+
+        private void OffCurrentGameStateObjects()
+        {
+            if (_currentGameStateObjects != null)
+            {
+                foreach (var gameStateObject in _currentGameStateObjects.Objects)
+                    gameStateObject.SetActive(false);
+            }
+        }
+
+        private void OnCurrentGameStateObjects()
+        {
+            if (_currentGameStateObjects != null)
+            {
+                foreach (var gameStateObject in _currentGameStateObjects.Objects)
+                    gameStateObject.SetActive(true);
+            }
+        }
+
+        private void SetGameState(GameStateObjects gameStateObjects)
+        {
+            OffCurrentGameStateObjects();
+            _currentGameStateObjects = gameStateObjects;
+            OnCurrentGameStateObjects();
+        }
+
+        public void LoadQuizGame()
+        {
+            SetGameState(_quizGame);
+        }
+
+        public void LoadLettesGame()
+        {
+            SetGameState(_lettersGame);
+        }
+
+        public void LoadObjectsGame()
+        {
+            SetGameState(_objectsGame);
+        }
+
+        public void LoadMenuGame()
+        {
+            SetGameState(_menuObjects);
+        }
+
+    }
+}
--- Assets/Scripts/Stages/FifthStage/Panels/DeviceDataPanel.cs
+++ Assets/Scripts/Stages/FifthStage/Panels/DeviceDataPanel.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using Zenject;
-using static UnityEditor.Progress;
 
 namespace Assets.Scripts.Stages.FifthStage.Panels
 {
